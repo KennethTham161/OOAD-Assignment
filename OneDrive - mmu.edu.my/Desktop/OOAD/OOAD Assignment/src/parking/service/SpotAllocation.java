bbe52277fcc36277c9405403ff2f3de0c3a4f395
散喵喵 <151101713+yunrulai@users.noreply.github.com>
2026-02-14 17:29:13 +0800

    Update SpotAllocation.java

--- OneDrive - mmu.edu.my/Desktop/OOAD/OOAD Assignment/src/parking/service/SpotAllocation.java
+++ OneDrive - mmu.edu.my/Desktop/OOAD/OOAD Assignment/src/parking/service/SpotAllocation.java
@@ -1,5 +1,26 @@
 package parking.service;
 
+import parking.model.*;
+import parking.data.DataCenter;
+import java.util.ArrayList;
+
 public class SpotAllocation {
 
+    public ArrayList<ParkingSpot> findSuitableSpots(Vehicle vehicle) {
+
+        ArrayList<ParkingSpot> result = new ArrayList<>();
+
+        for (Floor floor : DataCenter.getFloors()) {
+            for (ParkingSpot spot : floor.getSpots()) {
+
+                if (spot.getStatus() == SpotStatus.AVAILABLE
+                        && vehicle.canParkIn(spot.getType())) {
+
+                    result.add(spot);
+                }
+            }
+        }
+
+        return result;
+    }
 }
